308138f78b5b5e2140cc95fa08c89e51b46082a2
elieiradukunda <iradukundaelie71@gmail.com>
2025-08-30 20:45:56 +0200

    solving 7kyu

--- someArrows.js
+++ someArrows.js
@@ -2,3 +2,13 @@ function checkArrow(arrow){
     return arrow.some(arrow=>arrow.length>1)
 }
 console.log(checkArrow(["hello","wor","sa"]))
+
+
+// Digital Root: keep summing digits until one digit
+function digitalRoot(n) {
+  while (n > 9) {
+    n = n.toString().split('').reduce((a, b) => a + +b, 0);
+  }
+  return n;
+}
+console.log()
